2f52bee28fcdcb7dbf9459958b94f35798dd574a
TaeHyoungKwon <kth5604@gmail.com>
2020-10-24 23:38:33 +0900

    add models about book

--- src/book/models.py
+++ src/book/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+from config import settings
+
+
+class Book(models.Model):
+    name = models.CharField(max_length=200)
+    sub_name = models.CharField(max_length=200)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL)
+    description = models.TextField()
+    published_date = models.DateTimeField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
